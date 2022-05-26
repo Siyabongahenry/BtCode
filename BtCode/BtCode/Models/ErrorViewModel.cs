c73f2a9facf4265a302dd7a4a6fbd0191eda3fa4
siyabongahenry <siyabongahenry23@gmail.com>
2022-05-26 21:14:03 +0200

    web-update

--- BtCode/BtCode/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace BtCode.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
